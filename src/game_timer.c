38d2e0adff1dbe9e3087d5113282c4f442f31a6f
Michael Aganier <michaelaganier@gmail.com>
2023-08-14 02:33:27 -0400

    Go back to fixed delta

--- src/game_timer.c
+++ src/game_timer.c
@@ -11,29 +11,12 @@ void game_timer_fini(game_timer_t *timer)
 	(void)timer;
 }
 
-static f64 clamp_if_non_zero(
-	const f64 interval,
-	const f64 elapsed)
-{
-	assert(interval >= 0.0);
-	assert(elapsed >= 0.0);
-
-	if (interval == 0.0) {
-		return elapsed;
-	}
-	const f64 max_multiplier = 2;
-	const f64 max = interval * max_multiplier;
-	const f64 clamped = elapsed > max ? max : elapsed;
-	return clamped;
-}
-
 bool game_timer_tick(game_timer_t *timer, f64seconds dt, f64 interval)
 {
 	timer->counter.seconds += dt.seconds;
 	if (timer->counter.seconds >= interval) {
-		const f64 clamped_dt = clamp_if_non_zero(interval, timer->counter.seconds);
-		timer->counter.seconds = 0.f;
-		timer->dt.seconds = clamped_dt;
+		timer->counter.seconds -= interval;
+		timer->dt.seconds = interval;
 		return true;
 	}
 	return false;
