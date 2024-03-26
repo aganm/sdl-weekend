2938aa9d504062ca10c981e5c4a51ec5b1f7f5de
Michael Aganier <michaelaganier@gmail.com>
2024-03-26 00:44:37 -0400

    launch.sh script aborts if game folder is not found

--- launch.sh
+++ launch.sh
@@ -4,14 +4,19 @@
 
 game=$(basename $1)
 
-if [[ "$OSTYPE" == "msys" ]]; then
-	gen="MSYS Makefiles"
-else
-	gen="Unix Makefiles"
+if [ ! -d "$game" ]; then
+	echo "$(pwd)/$(basename $0): cannot find $game: No such file or directory"
+	exit
 fi
 
 if [ -f $game/standalone ]; then
 	standalone="Yes"
+fi
+
+if [[ "$OSTYPE" == "msys" ]]; then
+	gen="MSYS Makefiles"
+else
+	gen="Unix Makefiles"
 fi
 
 mkdir -p build && cd build
